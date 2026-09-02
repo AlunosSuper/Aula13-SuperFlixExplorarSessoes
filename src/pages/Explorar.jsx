952a5ad4870d03742db85ac08b4345137e6589fb
Aluno Super <alunossupergeeksjoinville@gmail.com>
2026-09-02 13:22:47 -0300

    continuação

--- src/pages/Explorar.jsx
+++ src/pages/Explorar.jsx
@@ -1,7 +1,36 @@
+import Navegacao from "../components/Navegacao"
+import Destaque from "../components/Destaque"
+import fundo from "../assets/fundo.jpg"
+import Sessao from "../components/Sessao"
+import ObterConteudos from "../functions/ObterConteudos"
+import ObterGeneros from "../functions/ObterGeneros"
+import { useEffect, useState } from "react"
+
+const [ conteudos, definirConteudos ] = useState([])
+const [ generos, definirGeneros ] = useState([])
+
+useEffect(function() {
+  ObterConteudos()
+    .then(function(resposta) {
+      if (resposta.status == 200)
+        definirConteudos(resposta.data)
+      else
+        console.log(resposta)
+    })  
+
+    .catch(function(erro) {
+      console.log(erro)
+  // alert(erro.message)
+})
+
+
+
+
+}, [])
+
+
 export default function Explorar() {
-  return (
-    <div>
-      <h1>Explorar</h1>
-    </div>
-  )
+  return <Destaque fundo={ fundo }>
+    <Navegacao />
+  </Destaque>
 }
